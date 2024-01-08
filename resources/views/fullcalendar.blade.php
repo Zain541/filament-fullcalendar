8f4ad3cbc06b87137f7a5bebbcb5c7b1c1fe2d0e
Zain Farooq <zainfarooq541@gmail.com>
2024-01-08 18:16:29 +0500

    Update fullcalendar.blade.php

--- resources/views/fullcalendar.blade.php
+++ resources/views/fullcalendar.blade.php
@@ -4,7 +4,10 @@
 
 <x-filament-widgets::widget>
     <x-filament::section>
-        <button id="toggleSidebarButton">Toggle Sidebar</button>
+
+    @php
+        // logger($this->draggableEvents());
+    @endphp
 
         <div class="flex justify-end flex-1 mb-4">
             <x-filament-actions::actions :actions="$this->getCachedHeaderActions()" class="shrink-0" />
@@ -12,8 +15,14 @@
         <div class="flex gap-2">
             <div class="flex flex-col collapsable-sidebar collapsed-sidebar" id="sidebar">
                 <div class="py-8"></div>
-                <div class="bg-white border border-gray-400 shadow-sm grow rounded-t-xl">
-                    <div id="mydraggable" data-event='{ "title": "my dragging event", "days": "10", "description": "This is my first dragging event", "start_time": "10:00", "end_time": "12:00", "user_id": "2", "color": "#FFFF00", "participants": ["1", "2", "3"], "eventable_type": "App\\Models\\Library\\Activity", "eventable_id": "10" }'>Drag me!</div>
+                <div class="flex flex-col gap-1 px-2 py-4 text-sm text-center text-white bg-white border border-gray-400 shadow-sm grow rounded-t-xl">
+                    @foreach ($this->draggableEvents() as $event)
+                        @php
+                            $eventColor = $event->color ?? '#D97706';
+                            $eventableType = str_replace("\\","\\\\", $event->eventable_type);
+                        @endphp
+                        <div class="cursor-move py-0.5 border rounded-md draggable" data-event='{"id": "{{ $event->id }}", "title": "working", "days": "1", "description": "{{ $event->description }}", "start_time": "{{ $event->start_time }}", "end_time": "{{ $event->end_time }}", "user_id": "{{ $event->user_id }}", "color": "{{ $eventColor }}", "participants": ["1", "2", "3"], "eventable_type": "{{ $eventableType }}", "eventable_id": "{{ $event->eventable_id }}" }'>{{ $event->name }}</div>
+                    @endforeach
                 </div>
             </div>
             <div class="grow">
@@ -44,6 +53,11 @@
             .collapsed-sidebar {
                 width: 0;
                 border: transparent !important;
+            }
+
+            .draggable{
+                background-color: #D97706;
+
             }
         </style>
     </x-filament::section>
