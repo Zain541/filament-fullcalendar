db891de1876850c001d4c5431df5072684651606
Zain Farooq <zainfarooq541@gmail.com>
2024-01-09 12:20:17 +0500

    Update FullCalendarWidget.php

--- src/Widgets/FullCalendarWidget.php
+++ src/Widgets/FullCalendarWidget.php
@@ -1,146 +1,65 @@
 <?php
 
-namespace Saade\FilamentFullCalendar\Widgets\Concerns;
+namespace Saade\FilamentFullCalendar\Widgets;
 
-use Carbon\Carbon;
-use Saade\FilamentFullCalendar\FilamentFullCalendarPlugin;
+use Filament\Actions\Action;
+use Filament\Actions\Concerns\InteractsWithActions;
+use Filament\Actions\Contracts\HasActions;
+use Filament\Forms\Concerns\InteractsWithForms;
+use Filament\Forms\Contracts\HasForms;
+use Filament\Widgets\Widget;
+use Saade\FilamentFullCalendar\Actions;
 
-trait InteractsWithEvents
+class FullCalendarWidget extends Widget implements HasForms, HasActions
 {
-    /**
-     * Triggered when the user clicks an event.
-     * @param array $event An Event Object that holds information about the event (date, title, etc).
-     * @return void
-     */
-    public function onEventClick(array $event): void
-    {
-        if ($this->getModel()) {
-            $this->record = $this->resolveRecord($event['id']);
-        }
+    use InteractsWithForms;
+    use InteractsWithActions;
+    use Concerns\InteractsWithEvents;
+    use Concerns\InteractsWithRecords;
+    use Concerns\InteractsWithHeaderActions;
+    use Concerns\InteractsWithModalActions;
 
-        $this->mountAction('view', [
-            'type' => 'click',
-            'event' => $event,
-        ]);
+    protected static string $view = 'filament-fullcalendar::fullcalendar';
+
+    protected int | string | array $columnSpan = 'full';
+
+    protected function headerActions(): array
+    {
+        return [
+            Actions\CreateAction::make(),
+        ];
+    }
+
+    protected function modalActions(): array
+    {
+        return [
+            Actions\EditAction::make(),
+            Actions\DeleteAction::make(),
+        ];
+    }
+
+    protected function viewAction(): Action
+    {
+        return Actions\ViewAction::make();
     }
 
     /**
-     * Triggered when dragging stops and the event has moved to a different day/time.
-     * @param array $event An Event Object that holds information about the event (date, title, etc) after the drop.
-     * @param array $oldEvent An Event Object that holds information about the event before the drop.
-     * @param array $relatedEvents An array of other related Event Objects that were also dropped. An event might have other recurring event instances or might be linked to other events with the same groupId
-     * @param array $delta A Duration Object that represents the amount of time the event was moved by.
-     * @return bool Wether to revert the drop action.
+     * FullCalendar will call this function whenever it needs new event data.
+     * This is triggered when the user clicks prev/next or switches views.
+     * @param array{start: string, end: string, timezone: string} $info
      */
-    public function onEventDrop(array $event, array $oldEvent, array $relatedEvents, array $delta): bool
+    public function fetchEvents(array $info): array
     {
-        if ($this->getModel()) {
-            $this->record = $this->resolveRecord($event['id']);
-        }
-
-        $this->mountAction('edit', [
-            'type' => 'drop',
-            'event' => $event,
-            'oldEvent' => $oldEvent,
-            'relatedEvents' => $relatedEvents,
-            'delta' => $delta,
-        ]);
-
-        return false;
+        return [];
     }
 
-    /**
-     * Triggered when resizing stops and the event has changed in duration.
-     * @param array $event An Event Object that holds information about the event (date, title, etc) after the drop.
-     * @param array $oldEvent An Event Object that holds information about the event before the drop.
-     * @param array $relatedEvents An array of other related Event Objects that were also dropped. An event might have other recurring event instances or might be linked to other events with the same groupId
-     * @param array $startDelta A Duration Object that represents the amount of time the event’s start date was moved by.
-     * @param array $endDelta A Duration Object that represents the amount of time the event’s end date was moved by.
-     * @return mixed Wether to revert the resize action.
-     */
-    public function onEventResize(array $event, array $oldEvent, array $relatedEvents, array $startDelta, array $endDelta): bool
+    public function getFormSchema(): array
     {
-        if ($this->getModel()) {
-            $this->record = $this->resolveRecord($event['id']);
-        }
-
-        $this->mountAction('edit', [
-            'type' => 'resize',
-            'event' => $event,
-            'oldEvent' => $oldEvent,
-            'relatedEvents' => $relatedEvents,
-            'startDelta' => $startDelta,
-            'endDelta' => $endDelta,
-        ]);
-
-        return false;
+        return [];
     }
 
-    /**
-     * Triggered when a date/time selection is made (single or multiple days).
-     * @param string $start An ISO8601 string representation of the start date. It will have a timezone offset similar to the calendar’s timeZone. If selecting all-day cells, it won’t have a time nor timezone part.
-     * @param string $end An ISO8601 string representation of the end date. It will have a timezone offset similar to the calendar’s timeZone. If selecting all-day cells, it won’t have a time nor timezone part.
-     * @param bool $allDay Whether the selection happened on all-day cells.
-     * @param array $view A View array that contains information about a calendar view, such as title and date range.
-     * @return void
-     */
-    public function onDateSelect(string $start, ?string $end, bool $allDay, ?array $view): void
+    public function draggableEvents()
     {
-        [$start, $end] = $this->calculateTimezoneOffset($start, $end, $allDay);
 
-        $this->mountAction('create', [
-            'type' => 'select',
-            'start' => $start,
-            'end' => $end,
-            'allDay' => $allDay,
-        ]);
-    }
-
-        /**
-
-     */
-    public function onDrop($dropInfo, $event): void
-    {
-        $date = $dropInfo['dateStr'];
-        $jsonEvent = json_decode($event);
-
-        $id = $jsonEvent->id;
-        $start_time = $jsonEvent->start_time;
-        $end_time = $jsonEvent->end_time;
-
-        $startDate = Carbon::parse($date)->format('Y-m-d');
-        $endDate = Carbon::parse($date)->format('Y-m-d');
-
-        $this->saveDraggingEvent([
-            'start_date' => $startDate,
-            'end_date' => $endDate,
-            'id' => $id,
-        ]);
-    }
-
-    public function refreshRecords(): void
-    {
-        $this->dispatch('filament-fullcalendar--refresh');
-    }
-
-    protected function calculateTimezoneOffset(string $start, ?string $end, bool $allDay): array
-    {
-        $timezone = FilamentFullCalendarPlugin::make()->getTimezone();
-
-        $start = Carbon::parse($start, $timezone);
-
-        if ($end) {
-            $end = Carbon::parse($end, $timezone);
-        }
-
-        if (! is_null($end) && $allDay) {
-            /**
-             * date is exclusive, read more https://fullcalendar.io/docs/select-callback
-             * For example, if the selection is all-day and the last day is a Thursday, end will be Friday.
-             */
-            $end->subDay()->endOfDay();
-        }
-
-        return [$start, $end, $allDay];
     }
 }
